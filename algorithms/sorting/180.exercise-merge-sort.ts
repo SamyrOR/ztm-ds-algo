a65ca5c0bef5773f2202c102cba6e2f095e65b29
Samyr Ribeiro <saamyr@live.com>
2023-10-22 00:18:50 -0300

    lesson: 180. Exercise: Merge Sort

---
+++ algorithms/sorting/180.exercise-merge-sort.ts
@@ -0,0 +1,34 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort(arr: number[]): number[] {
+  if (arr.length === 1) {
+    return arr;
+  }
+  // Split arr in into right and left
+  const length = arr.length;
+  const middle = Math.floor(length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left: number[], right: number[]) {
+  const result: number[] = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  //   console.log(left, right)
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const answer = mergeSort(numbers);
+console.log(answer);
